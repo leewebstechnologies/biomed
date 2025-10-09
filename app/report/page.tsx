ee03f214eb8d49596ef9bee2630a69c31175aaa6
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2025-10-09 06:06:21 -0700

    remove report's content

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,30 +1,10 @@
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 
-// const Report = () => {
-//   return (
-//     <div className={styles.container}>
-//       <aside className={styles.sidebar}>
-//         <h2>Tido Biomed</h2>
-//         <ul>
-//           <li className={styles.active}>Reports</li>
-//           <li>Inventory</li>
-//           <li>Activity</li>
-//           <li>Profile</li>
-//         </ul>
-//       </aside>
-
-//       <main className={styles.main}>
-//         <header className={styles.header}>
-//           <h3>Reports</h3>
-//         </header>
-
-//         <section className={styles.reportContainer}>
-//           <div className={styles.reportBox}>
-//             <p>No reports available yet.</p>
-//           </div>
-//         </section>
-//       </main>
-//     </div>
-//   );
-// }
-// export default Report
+const Report = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Report Page</h1>
+    </div>
+  );
+}
+export default Report
