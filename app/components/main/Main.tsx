c3ffeb1c61413d11bbbcb404fd759d167d54f55d
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2025-10-11 01:50:27 -0700

    add main

--- app/components/main/Main.tsx
+++ app/components/main/Main.tsx
@@ -5,24 +5,28 @@ const Main = () => {
   return (
     <div className={styles.main}>
       <div className={styles.welcome}>
-        <h3 className={styles.h3}>Welcome Back, Emmanuel 👋</h3>
-        <p className={styles.subtitle}>What would you like to do today?</p>
+        <h3 className={styles.h3}>
+          Welcome Back, <span>Emmanuel</span> 👋
+        </h3>
+        <p className={styles.subTitle}>What would you like to do today?</p>
       </div>
 
       <div className={styles.stock}>
         <div className={styles.stockCard}>
           <div className={styles.stockCardTop}>
             <h4 className={styles.stockTitle}>Total Supplies</h4>
-            <Image
-              src="/images/uil_medical.png"
-              width={24}
-              height={24}
-              alt="stock card icon"
-            />
+            <div className={styles.stockIcon}>
+              <Image
+                src="/images/uil_medical.png"
+                width={24}
+                height={24}
+                alt="stock card icon"
+              />
+            </div>
           </div>
           <div className={styles.stockCardBottom}>
             <p className={styles.stockValue}>1,200</p>
-            <p className={styles.stockReport}>
+            <p>
               <span className={styles.span}>+10%</span> Compared to Last week
             </p>
           </div>
@@ -30,12 +34,15 @@ const Main = () => {
         <div className={styles.stockCard}>
           <div className={styles.stockCardTop}>
             <h4 className={styles.stockTitle}>Low Stock</h4>
-            <Image
-              src="/images/alert.png"
-              width={24}
-              height={24}
-              alt="stock card icon"
-            />
+            <div className={styles.stockIcon}>
+              <Image
+                className={styles.stockIconImg}
+                src="/images/alert.png"
+                width={24}
+                height={24}
+                alt="stock card icon"
+              />
+            </div>
           </div>
           <div className={styles.stockCardBottom}>
             <p className={styles.stockValue}>20</p>
@@ -47,12 +54,15 @@ const Main = () => {
         <div className={styles.stockCard}>
           <div className={styles.stockCardTop}>
             <h4 className={styles.stockTitle}>Out of Stock</h4>
-            <Image
-              src="/images/trend.png"
-              width={24}
-              height={24}
-              alt="stock card icon"
-            />
+            <div className={styles.stockIcon}>
+              <Image
+                className={styles.stockIconImg}
+                src="/images/trend.png"
+                width={24}
+                height={24}
+                alt="stock card icon"
+              />
+            </div>
           </div>
           <div className={styles.stockCardBottom}>
             <p className={styles.stockValue}>20</p>
@@ -64,12 +74,15 @@ const Main = () => {
         <div className={styles.stockCard}>
           <div className={styles.stockCardTop}>
             <h4 className={styles.stockTitle}>Expiring Soon</h4>
-            <Image
-              src="/images/timer.png"
-              width={24}
-              height={24}
-              alt="stock card icon"
-            />
+            <div className={styles.stockIcon}>
+              <Image
+                className={styles.stockIconImg}
+                src="/images/timer.png"
+                width={24}
+                height={24}
+                alt="stock card icon"
+              />
+            </div>
           </div>
           <div className={styles.stockCardBottom}>
             <p className={styles.stockValue}>50</p>
@@ -80,13 +93,18 @@ const Main = () => {
         </div>
       </div>
       <div className={styles.cardTop}>
-        <div className={styles.cardLeft}></div>
-        <div className={styles.cardRight}></div>
+        <div className={styles.cardLeft}>
+
+        </div>
+        <div className={styles.cardRight}>
+
+        </div>
       </div>
 
-      <div className={styles.cardBottom}></div>
-
+      <div className={styles.cardBottom}>
+        
+      </div>
     </div>
   );
-}
-export default Main
+};
+export default Main;
